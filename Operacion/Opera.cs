049d6ac096caf556631e54d3e15fd279538380fa
Borja Zafra Aguilar <92036880+Boorja@users.noreply.github.com>
2022-03-01 22:11:26 +0100

    Add files via upload
    Añadida Solucion Operacion

---
+++ Operacion/Opera.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacion
+{
+    public class Opera
+    {
+        public double Suma(double num1,double num2)
+        {
+            return num1 + num2; 
+        }
+
+        public double Resta(double num1, double num2)
+        {
+            return num1 - num2; 
+        }
+
+        public double Division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public double Multiplicacion(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+
+    }
+}
